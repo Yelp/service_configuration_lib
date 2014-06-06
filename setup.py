574f7c50444b15f57af0190972455e633c5f0ed3
James McGuinness <jrm@yelp.com>
2014-06-06 14:02:52 -0700

    hopefully making the package work

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 
 setup(
     name='service_configuration_lib',
-    version='0.1',
+    version='0.1.1',
     provides=['service_configuration_lib'],
     description='Start, stop, and inspect Yelp SOA services',
+    url='https://gitweb.yelpcorp.com/?p=service_configuration_lib.git',
     author='Yelp Operations Team',
     author_email='operations@yelp.com',
     packages=find_packages(exclude=['tests']),
-    install_requires=[],
+    setup_requires=['setuptools'],
+    install_requires=['PyYAML >= 3.0'],
+    license='Copyright Yelp 2013, All Rights Reserved'
 )
