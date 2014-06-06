a6cc8669b28fe5f64b94982bfbc55812bbcecc27
James McGuinness <jrm@yelp.com>
2014-06-06 14:02:52 -0700

    hopefully making the package work

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
 PYTHON_PKGNAME = service_configuration_lib
 
-.PHONY: tests
+.PHONY: all production test tests coverage clean
 
-test: tests
+all: production
 
-tests:
+production:
+	@true
+
+test:
 	tox
+
+tests: test
+coverage: test
+
+clean:
+	find . -name '*.pyc' -delete
+	find . -name '__pycache__' -delete
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 
 setup(
     name='service_configuration_lib',
-    version='0.1',
+    version='0.1.1',
     provides=['service_configuration_lib'],
     description='Start, stop, and inspect Yelp SOA services',
+    url='https://github.com/Yelp/service_configuration_lib',
     author='Yelp Operations Team',
     author_email='opensource+scl@yelp.com',
     packages=find_packages(exclude=['tests']),
-    install_requires=[],
+    setup_requires=['setuptools'],
+    install_requires=['PyYAML >= 3.0'],
+    license='Copyright Yelp 2013, All Rights Reserved'
 )
