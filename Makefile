f27169aa7350931cf0bc189c9e1f074cc5ad41cd
James McGuinness <jrm@yelp.com>
2014-06-10 15:55:06 -0700

    moving things to work

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 PYTHON_PKGNAME = service_configuration_lib
+UID:=`id -u`
+GID:=`id -g`
+DOCKER_RUN:=docker run -t -v  $(CURDIR):/work:rw lucid_container
 
 .PHONY: all production test tests coverage clean
 
@@ -13,6 +16,23 @@ test:
 tests: test
 coverage: test
 
+itest_lucid: package_lucid
+	$(DOCKER_RUN) /bin/bash -c "dpkg -i /work/dist/*.deb && python /work/tests/*.py"
+
+package_lucid: test_lucid
+	$(DOCKER_RUN) /bin/bash -c "./package-python yelp1 . && mv *.deb dist/"
+	$(DOCKER_RUN) chown -R $(UID):$(GID) /work
+
+test_lucid: build_lucid_docker
+	$(DOCKER_RUN) bash -c "tox"
+	$(DOCKER_RUN) chown -R $(UID):$(GID) /work
+
+build_lucid_docker:
+	[ -d dist ] || mkdir dist
+	cd dockerfiles/lucid/ && docker build -t "lucid_container" .
+
 clean:
 	find . -name '*.pyc' -delete
 	find . -name '__pycache__' -delete
+	rm -rf dist/
+	rm -rf .tox
--- setup.py
+++ setup.py
@@ -3,14 +3,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='service_configuration_lib',
-    version='0.1.1',
+    name='service-configuration-lib',
+    version='0.1.2',
     provides=['service_configuration_lib'],
     description='Start, stop, and inspect Yelp SOA services',
     url='https://github.com/Yelp/service_configuration_lib',
     author='Yelp Operations Team',
     author_email='opensource+scl@yelp.com',
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=['tests', 'scripts']),
     install_requires=['PyYAML >= 3.0'],
     license='Copyright Yelp 2013, All Rights Reserved',
+    scripts=[
+        'scripts/all_nodes_that_run',
+        'scripts/dump_service_configuration_yaml',
+        'scripts/services_deployed_here',
+        'scripts/services_needing_puppet_help',
+        'scripts/services_that_run_here',
+        'scripts/services_using_ssl'
+    ],
 )
