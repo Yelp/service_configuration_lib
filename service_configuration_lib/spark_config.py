62105a86a20d285d3335ae436527ae2b72cb3b3e
Sameer Sharma <sameersharma@yelp.com>
2022-08-11 07:18:17 -0700

    MLCOMPUTE-439 | version bump up

--- service_configuration_lib/spark_config.py
+++ service_configuration_lib/spark_config.py
@@ -430,9 +430,11 @@ def _adjust_spark_requested_resources(
         # TODO(gcoll|COREML-2697): Consider cleaning this part of the code up
         # once mesos is not longer around at Yelp.
         if 'spark.executor.instances' not in user_spark_opts:
-
+            # spark.cores.max provided, calculate based on (max cores // per-executor cores)
             if 'spark.cores.max' in user_spark_opts:
                 executor_instances = int(user_spark_opts['spark.cores.max']) // executor_cores
+            # spark.executor.instances and spark.cores.max not provided, the executor instances should at least
+            # be equal to DEFAULT_EXECUTOR_INSTANCES.
             else:
                 executor_instances = max(DEFAULT_MAX_CORES // executor_cores, DEFAULT_EXECUTOR_INSTANCES)
             user_spark_opts['spark.executor.instances'] = str(executor_instances)
