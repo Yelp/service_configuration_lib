2589a2bef86bbefd32082a3c622ce4291e4e500a
Chi Chang <edingroot@users.noreply.github.com>
2023-10-02 15:39:39 +0200

    Show warning message when spark app id is explicitly specified (#129)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='service-configuration-lib',
-    version='2.18.7',
+    version='2.18.8',
     provides=['service_configuration_lib'],
     description='Start, stop, and inspect Yelp SOA services',
     url='https://github.com/Yelp/service_configuration_lib',
