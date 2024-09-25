ef95386b9aadac87d9b108a99e6a3520c9365941
Jason Sleight <jsleight@yelp.com>
2024-09-25 13:38:25 -0700

    Release v2.18.23

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='service-configuration-lib',
-    version='2.18.22',
+    version='2.18.23',
     provides=['service_configuration_lib'],
     description='Start, stop, and inspect Yelp SOA services',
     url='https://github.com/Yelp/service_configuration_lib',
